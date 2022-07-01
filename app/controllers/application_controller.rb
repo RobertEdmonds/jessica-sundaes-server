46aa756b55a1389293e5a0f3ca97d6f8ebbc098f
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-06-30 23:14:37 -0600

    Add get request for sundae, add get request for ingredients in sundae

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/sundae' do
+    sundae = Sundae.all
+    sundae.to_json
+  end
+  get '/sundae_ingredients/:id' do
+    sundae = Sundae.find(params[:id])
+    sundae_ingred = sundae.ingredient_array
+    sundae_ingred.to_json
   end
 
 end
--- app/models/sundae.rb
+++ app/models/sundae.rb
@@ -1,4 +1,17 @@
+
 class Sundae < ActiveRecord::Base
     has_many :sundae_reviews
+
+    def ingredient_array
+        # base = []
+        # self.all.each do |sundae|
+        #     ingredient = sundae.ingredients
+        #     array = ingredient.split(',')
+        #     base << array
+        # end
+        # base
+        ingredient = self.ingredients
+        ingredient.split(',')
+    end
 
 end
