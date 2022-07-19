693792bed8b23b638bc4b079b863d2ee9727a95b
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-07-19 16:29:07 -0400

    Fix the resting routes for sundae and shakes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,50 +2,48 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Sundae
-  get '/sundae' do
+  get '/sundaes' do
     sundae = Sundae.all
     sundae.to_json
   end
-  get '/sundae_ingredients/:id' do
-    sundae = Sundae.find(params[:id])
-    sundae_ingred = sundae.ingredient_array
-    sundae_ingred.to_json
-  end
   patch '/sundae/:id' do
     sundae = Sundae.find(params[:id])
-    sundae.update(likes: params[:likes] + 1)
-    all_sundae = Sundae.all
-    all_sundae.to_json
+    sundae.update(likes: params[:likes])
+    sundae.to_json
   end
   # Sundae Reviews 
-  get '/sundae_reviews/:id' do 
+  get '/sundae/:id' do 
     sundae = Sundae.find(params[:id])
-    reviews = sundae.sundae_reviews.all 
+    reviews = sundae.sundae_reviews
     reviews.to_json
   end
   # Shake
-  get '/shake' do
+  # add plural routes, remove ingred
+  get '/shakes' do
     shake = Shake.all
     shake.to_json
   end
-  get '/shake_ingredients/:id' do
-    shake = Shake.find(params[:id])
-    shake_ingred = shake.ingredient_array
-    shake_ingred.to_json
-  end
   patch '/shake/:id' do
     shake = Shake.find(params[:id])
-    shake.update(likes: params[:likes] + 1)
+    shake.update(likes: params[:likes])
     all_shake = Shake.all
     all_shake.to_json
   end
   # Shake Reviews
-  get '/shake_reviews/:id' do 
+  # /shakes/:shake_id/shake_reviews
+  # restful routs
+  # 
+  get '/shake/:id' do 
     shake = Shake.find(params[:id])
-    reviews = shake.shake_reviews.all 
+    reviews = shake.shake_reviews 
     reviews.to_json
   end
-
+  patch '/shake_reviews/:id' do
+    review = Shake_reviews.find(params[:id])
+    review.update(likes: params[:likes] + 1)
+    all_reviews = Shake_reviews.all
+    all_reviews.to_json
+  end
 
 
 end
